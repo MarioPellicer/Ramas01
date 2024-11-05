2584874d0f692088f2078e831dc169b675bfc546
mariopellicer <marioopelli23@gmail.com>
2024-11-05 12:00:47 +0100

    funciones agregar nota,calcularPromedio,notaMaxima,notaMinima

--- Ramas01/src/Ejercicio/Rnotas.java
+++ Ramas01/src/Ejercicio/Rnotas.java
@@ -1,19 +1,26 @@
 package Ejercicio;
 
 public class Rnotas {
-		static int todo=0;
+		static double todo=0;
 		static int contador=0;
-		static int mayor=0;
-		static int menor=0;
+		static double mayor=0;
+		static double menor=0;
 	public static void main(String[] args) {
 
 	}
 		public static void agregarNota(double nota) {
-		todo=
+		todo=todo+nota;
 		contador++;
-		mayor=Math.max(mayor, nota);
-		menor=Math.min(menor, nota);
+		mayor= Math.max(mayor, nota);
+		menor= Math.min(menor, nota);
 		}
-		
-		
+		public static double calcularPromedio(int totalAlumnos) {
+			return todo/contador;
+		}
+		public static double notaMaxima()  {
+			return mayor;
+		}
+		public static double notaMinima() {
+			return menor;
+		}
 }
